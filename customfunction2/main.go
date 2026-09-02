95e96a292508fd382265e379d1d829364947c056
Dhanalakshmi-D04 <dhanalakshmidgowda04@gmail.com>
2026-09-02 23:18:35 +0530

    changed few code

--- customfunction2/main.go
+++ customfunction2/main.go
@@ -5,10 +5,25 @@ import (
 )
 
 func swap(x, y string) (string, string) {
-	return x, y
+	return y, x
 }
 
 func main() {
 	x, y := swap("hello", "world")
 	fmt.Println(x, y)
+
+	main1()
+}
+
+func swap1(a, b string) (string, string) {
+	temp := a
+	a = b
+	b = temp
+
+	return a, b
+}
+
+func main1() {
+	a, b := swap1("he", "wo")
+	fmt.Println(a, b)
 }
